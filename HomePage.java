4ad8a9cc35ce76e432c7774194fb3999be84de4a
christianher89 <131484670+christianher89@users.noreply.github.com>
2024-10-16 16:56:53 -0700

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -6,15 +6,16 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HomePage extends Application{
+
 @Override
 public void start(Stage primary) {
 // TODO Auto-generated method stub
-BorderPane root = new BorderPane();
-Scene scene = new Scene(root,1024,600);
-scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-primary.setScene(scene);
-primary.show();
 try {
+	BorderPane root = new BorderPane();
+	Scene scene = new Scene(root,1024,600);
+	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	primary.setScene(scene);
+	primary.show();
 } catch (Exception e) {
 e.printStackTrace();
 	}
