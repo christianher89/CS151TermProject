ecdcc039f198589fe347182e76d31e74633af797
christianher89 <131484670+christianher89@users.noreply.github.com>
2024-10-16 16:47:31 -0700

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,25 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class HomePage extends Application{
+
+@Override
+public void start(Stage primary) {
+// TODO Auto-generated method stub
+BorderPane root = new BorderPane();
+Scene scene = new Scene(root,1024,600);
+scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+primary.setScene(scene);
+primary.show();
+try {
+	
+} catch (Exception e) {
+e.printStackTrace();
+	}
+}
+
+}
