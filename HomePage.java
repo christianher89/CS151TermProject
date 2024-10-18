b6cdbd2db45559807354c4190ca471feb5f31eaf
Ryan-Kozel <133525567+Ryan-Kozel@users.noreply.github.com>
2024-10-17 19:49:40 -0700

    Update HomePage.java
    I put a button on the home page that when clicked sets the scene to the accountPage. On the accountPage, there is a button that when clicked take you back to the homePage

--- HomePage.java
+++ HomePage.java
@@ -1,25 +1,53 @@
 package application;
 
+import javafx.scene.control.Button;
 import javafx.application.Application;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 
 public class HomePage extends Application{
+	private BorderPane mainPage = new BorderPane();
+	private Button homeBtn = new Button("Create an account");
 
-@Override
-public void start(Stage primary) {
-// TODO Auto-generated method stub
-BorderPane root = new BorderPane();
-Scene scene = new Scene(root,1024,600);
-scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-primary.setScene(scene);
-primary.show();
-try {
-	fdkljdfklkllkdfgklgdflkgdflkgdflk
-} catch (Exception e) {
-e.printStackTrace();
+
+	@Override
+	public void start(Stage primary) {
+		try {
+			mainPage.setCenter(homeBtn);
+			
+			homeBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> openAccountPage(primary));
+			
+			
+			Scene homeScene = new Scene(mainPage,1024,600);
+			homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primary.setScene(homeScene);
+			primary.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-}
+
+	private void openAccountPage(Stage primary) {	
+		BorderPane ap = new BorderPane();
+		
+		Button back = new Button("Home");
+		ap.setCenter(back);
+		
+		back.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> backToHomePage(primary));
+		
+		Scene accountPage = new Scene(ap, 1024, 600);
+		
+		primary.setScene(accountPage);
+		primary.show();
+		
+	}
+	
+	private void backToHomePage(Stage primary) {
+		HomePage hp = new HomePage();
+		hp.start(primary);
+	}
 
 }
