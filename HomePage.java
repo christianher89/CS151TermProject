27314bde11f4326f728a82e0061866885cee913b
christianher89 <131484670+christianher89@users.noreply.github.com>
2024-10-19 20:03:33 -0700

    Update HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -15,31 +15,40 @@ import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
 
 public class HomePage extends Application{
-    private BorderPane mainPage = new BorderPane();
+    private BorderPane pages = new BorderPane();
     private Button homeBtn = new Button("Create an account");
+    private Text title = new Text("PennyPal");
 
     @Override
     public void start(Stage primary) {
         try {
-            VBox content = new VBox(20);
+            Text welcome = new Text("Welcome to");
+            
+            title.setStyle("-fx-font-size: 50px; -fx-font-weight: bold;");
+            welcome.setStyle("-fx-font-size: 25px;");
+            homeBtn.setMinSize(150, 35);
 
-            Text title = new Text("PennyPal");
-            Text welcome = new Text("Welcome to PennyPal");
-            title.setStyle("-fx-font-size: 36px; -fx-font-weight: bold;");
-            welcome.setStyle("-fx-font-size: 18px;");
+            VBox centerContent = new VBox(homeBtn);
+            centerContent.setAlignment(Pos.CENTER);
+            centerContent.setPadding(new Insets(-100, 0, 0, 0));
+            
+            VBox topContent = new VBox(10, welcome, title);
+            topContent.setAlignment(Pos.TOP_CENTER);
+            topContent.setPadding(new Insets(150, 0, 0, 0));
+            
 
-            content.getChildren().addAll(title, welcome, homeBtn);
-            content.setAlignment(Pos.CENTER);
-
-            mainPage.setCenter(content);
+            pages.setTop(topContent);
+            pages.setCenter(centerContent);
 
             homeBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> openAccountPage(primary));
 
 
-            Scene homeScene = new Scene(mainPage,1024,600);
+            Scene homeScene = new Scene(pages,1280,800);
             homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            
             primary.setScene(homeScene);
             primary.show();
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -48,21 +57,53 @@ public class HomePage extends Application{
     private void openAccountPage(Stage primary) {
         GridPane ap = new GridPane();
 
+        Text createAccount = new Text("Please create account:");
         TextField accName = new TextField();
         TextField accBal = new TextField();
         TextField accDate = new TextField();
-
+        Label nameLabel = new Label("Account name:");
+        Label date = new Label("Opening date:");
+        Label balance = new Label("Opening balance:");
         Button back = new Button("Home");
-        ap.add(back, 0,  0);
-        ap.add(accName, 0, 3);
-        ap.add(accBal, 1, 3);
-        ap.add(accDate, 2, 3);
+        Button create = new Button("Create");
+        create.setMinSize(100, 30);
+        
+        nameLabel.setStyle("-fx-font-size: 15;");
+        date.setStyle("-fx-font-size: 15;");
+        balance.setStyle("-fx-font-size: 15;");
+        createAccount.setStyle("-fx-font-size: 30; -fx-font-weight: bold;");
+        title.setStyle("-fx-font-size: 50px; -fx-font-weight: bold;");
+        
+        ap.add(accName, 0, 1);
+        ap.add(accBal, 1, 1);
+        ap.add(accDate, 2, 1);
+        ap.add(nameLabel, 0, 0);
+        ap.add(date, 1, 0);
+        ap.add(balance, 2, 0);
+        
+        ap.setHgap(250);
+        ap.setVgap(5);
+        ap.setAlignment(Pos.BOTTOM_CENTER);
+        ap.setPadding(new Insets(0, 70, 70, 0));
+        
+        VBox homeButtonContainer = new VBox(back);
+        homeButtonContainer.setPadding(new Insets(0, 0, 0, 20));
+        
+        VBox centerContent = new VBox(175, title, createAccount);
+        centerContent.setAlignment(Pos.TOP_CENTER);
+        centerContent.setPadding(new Insets(194, 0, 0, 0));
+        
+        VBox createButtonContainer = new VBox(create);
+        createButtonContainer.setAlignment(Pos.BOTTOM_CENTER);
+        createButtonContainer.setPadding(new Insets(0, 0, 100, 0));
+        
+        pages.setLeft(homeButtonContainer);
+        pages.setTop(centerContent);
+        pages.setCenter(ap);
+        pages.setBottom(createButtonContainer);
 
         back.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> backToHomePage(primary));
 
-        Scene accountPage = new Scene(ap, 1024, 600);
-
-        primary.setScene(accountPage);
         primary.show();
 
     }
