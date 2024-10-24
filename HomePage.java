814fc692c5c0eef1ca7dbe83110d4aaeb49a1b91
Ryan-Kozel <133525567+Ryan-Kozel@users.noreply.github.com>
2024-10-24 11:05:30 -0700

    Update HomePage.java
    In HomePage I fixed the textField labels to match the right field. I then added a simple EventHandler to take the typed data and create a new account using the new Account class. This will store the data in Accounts.csv. We still need to make sure the input is formatted correctly and all that fun stuff.

--- HomePage.java
+++ HomePage.java
@@ -1,7 +1,16 @@
 package application;
 
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.input.MouseEvent;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+import java.io.FileNotFoundException;
+
 import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -11,51 +20,118 @@ import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
 
 public class HomePage extends Application{
-	private BorderPane mainPage = new BorderPane();
-	private Button homeBtn = new Button("Create an account");
+    private BorderPane pages = new BorderPane();
+    private Button homeBtn = new Button("Create an account");
+    private Text title = new Text("PennyPal");
 
-	@Override
-	public void start(Stage primary) {
-		try {
-			mainPage.setCenter(homeBtn);
-			
-			homeBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> openAccountPage(primary));
-			
-			
-			Scene homeScene = new Scene(mainPage,1024,600);
-			homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primary.setScene(homeScene);
-			primary.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void start(Stage primary) {
+        try {
+            Text welcome = new Text("Welcome to");
 
-	private void openAccountPage(Stage primary) {	
-		GridPane ap = new GridPane();
-		
-		TextField accName = new TextField();
-		TextField accBal = new TextField();
+            title.setStyle("-fx-font-size: 50px; -fx-font-weight: bold;");
+            welcome.setStyle("-fx-font-size: 25px;");
+            homeBtn.setMinSize(150, 35);
 
-		
-		Button back = new Button("Home");
-		ap.add(back, 0,  0);
-		ap.add(accName, 0, 3);
-		ap.add(accBal, 1, 3);
-		ap.add(accDate, 2, 3);
-		
-		back.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> backToHomePage(primary));
-		
-		Scene accountPage = new Scene(ap, 1024, 600);
-		
-		primary.setScene(accountPage);
-		primary.show();
-		
-	}
-	
-	private void backToHomePage(Stage primary) {
-		HomePage hp = new HomePage();
-		hp.start(primary);
-  }
+            VBox centerContent = new VBox(homeBtn);
+            centerContent.setAlignment(Pos.CENTER);
+            centerContent.setPadding(new Insets(-100, 0, 0, 0));
+
+            VBox topContent = new VBox(10, welcome, title);
+            topContent.setAlignment(Pos.TOP_CENTER);
+            topContent.setPadding(new Insets(150, 0, 0, 0));
+
+
+            pages.setTop(topContent);
+            pages.setCenter(centerContent);
+
+            homeBtn.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> openAccountPage(primary));
+
+
+            Scene homeScene = new Scene(pages,1280,800);
+            homeScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+            primary.setScene(homeScene);
+            primary.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openAccountPage(Stage primary) {
+        GridPane ap = new GridPane();
+
+        Text createAccount = new Text("Please create account:");
+        TextField accName = new TextField();
+        TextField accBal = new TextField();
+        TextField accDate = new TextField();
+        Label nameLabel = new Label("Account name:");
+        Label date = new Label("Opening date:");
+        Label balance = new Label("Opening balance:");
+        Button back = new Button("Home");
+        Button create = new Button("Create");
+        create.setMinSize(100, 30);
+
+        nameLabel.setStyle("-fx-font-size: 15;");
+        date.setStyle("-fx-font-size: 15;");
+        balance.setStyle("-fx-font-size: 15;");
+        createAccount.setStyle("-fx-font-size: 30; -fx-font-weight: bold;");
+        title.setStyle("-fx-font-size: 50px; -fx-font-weight: bold;");
+
+        ap.add(accName, 0, 1);
+        ap.add(accBal, 1, 1);
+        ap.add(accDate, 2, 1);
+        ap.add(nameLabel, 0, 0);
+        ap.add(date, 2, 0);
+        ap.add(balance, 1, 0);
+
+        ap.setHgap(250);
+        ap.setVgap(5);
+        ap.setAlignment(Pos.BOTTOM_CENTER);
+        ap.setPadding(new Insets(0, 70, 70, 0));
+
+        VBox homeButtonContainer = new VBox(back);
+        homeButtonContainer.setPadding(new Insets(0, 0, 0, 20));
+
+        VBox centerContent = new VBox(175, title, createAccount);
+        centerContent.setAlignment(Pos.TOP_CENTER);
+        centerContent.setPadding(new Insets(194, 0, 0, 0));
+
+        VBox createButtonContainer = new VBox(create);
+        createButtonContainer.setAlignment(Pos.BOTTOM_CENTER);
+        createButtonContainer.setPadding(new Insets(0, 0, 100, 0));
+
+        pages.setLeft(homeButtonContainer);
+        pages.setTop(centerContent);
+        pages.setCenter(ap);
+        pages.setBottom(createButtonContainer);
+
+        
+        back.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> backToHomePage(primary));
+        
+        //Gathers data from the 3 textFields, creates a new account with that data and stores it in Accounts.csv
+        create.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+        	String name = accName.getText();
+        	Double balD = Double.parseDouble(accBal.getText());
+        	double bal = Double.valueOf(balD);
+        	String openDate = accDate.getText();
+        	        	
+        	Account newAcc  = new Account(name, bal, openDate);
+        	try {
+				Account.storeData(newAcc);
+			} catch (FileNotFoundException e1) {
+				e1.printStackTrace();
+			}
+        });
+
+        primary.show();
+
+    }
+
+    private void backToHomePage(Stage primary) {
+        HomePage hp = new HomePage();
+        hp.start(primary);
+    }
 
 }
