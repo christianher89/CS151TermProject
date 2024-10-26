abf032d008c3bf0b23f99ccf147669e6f1d66176
christianher89 <131484670+christianher89@users.noreply.github.com>
2024-10-25 22:27:34 -0700

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,15 +1,18 @@
 package application;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.*;
 
 public class Account {
 	private String accName = "";
     private double startBal = 0;
     private String openDate = "";
+    private static ArrayList<Account> accountList = new ArrayList<Account>();
 
     public Account (String name, double bal, String date) {
         accName = name;
@@ -27,35 +30,53 @@ public class Account {
         return openDate;
     }
 
-    public static void storeData(Account newAcc) throws FileNotFoundException {
+    public static void storeData(Account newAcc) throws IOException {
         // TODO Auto-generated method stub
+    	accountList.add(newAcc);
+    	
         File accounts = new File("Accounts.csv");
+        if (!accounts.exists()) {
+        	accounts.createNewFile();
+        }
         PrintWriter out = new PrintWriter(accounts);
-
-        out.print(newAcc);
+        
+        for (Account a : accountList) {
+        	out.println(newAcc.getName() + "," + newAcc.getBalance() + "," + newAcc.getOpenDate());
+        }
         out.close();
 
     }
-    public static void appndData(Account newAcc) throws FileNotFoundException{
+    public static void appndData(Account newAcc) throws IOException{
         File accounts = new File("Account.csv");
         PrintWriter out = new PrintWriter(new FileOutputStream(accounts, true));
 
         out.println(newAcc.getName() + "," + newAcc.getBalance() + "," + newAcc.getOpenDate());
         out.close();
     }
-    public static List<Account> getAllAccounts() throws FileNotFoundException{
+    public static List<Account> getAllAccounts() throws IOException{
         List<Account> accounts = new ArrayList<>();
-        try(Scanner sc = new Scanner(new File("Accounts.csv"))){
-            while(sc.hasNextLine()){
-                String[] data = sc.nextLine().split(",");
-                if(data.length == 3) accounts.add(new Account(data[0], Double.parseDouble(data[1]), data[2]));
+        try(BufferedReader reader = new BufferedReader(new FileReader("Accounts.csv"))){
+        	String line = "";
+            while((line = reader.readLine()) != null){
+                String[] data = line.split(",");
+                if(data.length == 3) {
+                	try {
+                		String name = data[0].trim();
+                		double balance = Double.parseDouble(data[1].trim());
+                		String date = data[2].trim();
+                		accounts.add(new Account(name, balance, date));
+                	} catch (NumberFormatException e) {
+                		System.out.println("Skipping line due to number format error:" + line);
+                		e.printStackTrace();
+                	}
+                }
 
             }
         }
         return accounts;
     }
 
-    public static List<Account> getSortedAccountsByDate() throws FileNotFoundException{
+    public static List<Account> getSortedAccountsByDate() throws IOException{
         List<Account> accounts = getAllAccounts();
         accounts.sort(Comparator.comparing(Account::getOpenDate).reversed());
         return accounts;
