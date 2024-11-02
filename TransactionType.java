7e4b6848bb39df63e6583e6d4d4a1128da473f1e
jarretthampel <151235725+jarretthampel@users.noreply.github.com>
2024-11-01 19:39:10 -0700

    Update TransactionType.java

--- TransactionType.java
+++ TransactionType.java
@@ -1,13 +1,21 @@
 package application;
+import java.io.*;
+import java.io.PrintWriter;
 
 public class TransactionType {
-	private String name = "";
-	
-	public TransactionType(String typeName) {
-		name = typeName;
-	}
-	
-	public String getName() {
-		return name;
-	}
+    private String name = "";
+
+    public TransactionType(String typeName) {
+        name = typeName;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public static void storeTransactionType(TransactionType newType) throw IOException{
+        File typesFile = new File("TransactionTypes.cvs");
+        try (PrintWriter out = new PrintWriter(new FileWriter(typesFile, true))){
+            out.println(newType.getName());
+        }
+    }
 }
