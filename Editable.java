ed834662855c6ea35750b1d27c3042a3fb459718
christianher89 <131484670+christianher89@users.noreply.github.com>
2024-11-23 21:41:53 -0800

    Add files via upload

---
+++ Editable.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package application;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 
+ */
+public interface Editable {
+	
+	public List<String> getEditableFields();
+	
+	public void save() throws IOException;
+}
